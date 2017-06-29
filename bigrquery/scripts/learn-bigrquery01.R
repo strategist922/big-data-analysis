845e78f9aa484a936cb4aee33c5c04759f7b52d0
Ashish Dutt <ashish.dutt8@gmail.com>
2017-06-29 11:58:02 +0800

    add sql code for querying data

--- bigrquery/scripts/learn-bigrquery01.R
+++ bigrquery/scripts/learn-bigrquery01.R
@@ -1,4 +1,16 @@
 library(bigrquery)
 project<- "voyage-2017"
-sql <- "SELECT year, month, day, weight_pounds FROM [publicdata:samples.natality] LIMIT 5"
-query_exec(sql, project = project)
+sql <- "SELECT year, month, day, weight_pounds FROM [publicdata:samples.natality]
+DESC LIMIT 10 "
+data.df<-query_exec(sql, project = project)
+str(data.df)
+
+# Querying sample dataset containing a list of wikipedia entries.
+sql <- 'SELECT title,contributor_username,comment FROM[publicdata:samples.wikipedia] WHERE title CONTAINS "beer" LIMIT 100;'
+
+data <- query_exec(sql, project = project)
+str(data)
+
+# List all tables in publicdata
+??list_tables
+list_tables("publicdata","samples")
