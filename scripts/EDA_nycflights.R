790b939ef6f8c7caff07ad63d483635e343827fc
Ashish Dutt <ashish.dutt8@gmail.com>
2017-06-29 11:57:08 +0800

    add code for checking data dimension and amount of memory taken by spark local cluster instance

--- scripts/EDA_nycflights.R
+++ scripts/EDA_nycflights.R
@@ -5,7 +5,12 @@
 # connecting to spark local cluster
 sc <- spark_connect(master = "local")
 
-flights_tbl <- copy_to(sc, nycflights13::flights, "flights")
+flights_tbl <- copy_to(sc, nycflights13::flights, "flights", overwrite = TRUE)
+# check data dimension
+dim(flights_tbl)
+# check amount of memory taken up by the flights_tbl tibble
+object.size(flights_tbl)
+# dplyr usage
 airportcounts <- flights_tbl %>% 
   filter(dest %in% c('ALB', 'BDL', 'BTV')) %>%
   group_by(year, month, dest) %>%
@@ -29,10 +34,7 @@ ggplot(delay, aes(dist, delay)) +
   geom_smooth(method = "loess") +
   scale_size_area(max_size = 2)
 
-# To view the spark web console
-spark_web(sc)
-# To see the log use the spark_log()
-spark_log(sc, n=10)
+
 
 # stop the spark local cluster
 spark_disconnect(sc)
