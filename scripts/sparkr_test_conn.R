a16eb711366d70ffac06beeea73ba2730fedd880
Ashish Dutt <ashish.dutt8@gmail.com>
2017-06-29 12:00:10 +0800

    modified code to contain only the steps to connect to local spark instance and shutdown

--- scripts/sparkr_test_conn.R
+++ scripts/sparkr_test_conn.R
@@ -1,15 +1,29 @@
 # This script is to be used for testing sparkr and sparklyr connection on standalone pc.
 # script created on: 27/5/2017
 
+# install spark on local machine, if not already installed
+sparklyr::spark_install()
+
 # Load the libraries
 library(sparklyr)
-library(nycflights13)
-
 #details of my session
 sessionInfo()
 
-# install spark on local machine
-sparklyr::spark_install()
-
 # Start spark cluster on local machine
 sc<- spark_connect(master = "local")
+# print the spark version
+spark_version(sc)
+
+# See which data frames are available in Spark, using 
+src_tbls(sc)
+
+# To view the spark web console
+spark_web(sc)
+# To see the log use the spark_log()
+spark_log(sc, n=10)
+
+# disconnect spark connection
+spark_disconnect(sc)
+
+
+
